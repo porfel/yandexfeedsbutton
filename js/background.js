d028f63483935b3ce78e77ec4cee2c1eff0c2c34
Dmitry Parfenov <porfel@porfel.ru>
2014-11-13 15:08:28 +0300

    Finnaly up to version 1.0.2

--- js/background.js
+++ js/background.js
@@ -8,6 +8,7 @@ chrome.browserAction.onClicked.addListener(function () {
         chrome.tabs.create({url: "https://mail.yandex.ru/my/#lenta/group/all/unread",selected: true}, function() {});
         count = 0;
         displayFeed();
+    setTimeout('checkFeed()', 60000);
     } 
 });
 
@@ -25,29 +26,35 @@ function displayFeed(){
     var icon_text = count > 0 ? "" + count : "";
     
     chrome.browserAction.setTitle({title: badgeText });
-	chrome.browserAction.setBadgeText({text: icon_text });
-	chrome.browserAction.setIcon({ path: icon });
+    chrome.browserAction.setBadgeText({text: icon_text });
+    chrome.browserAction.setIcon({ path: icon });
     chrome.browserAction.setBadgeBackgroundColor({ color: '#ff536a' });
 }
 
 function checkFeed(){
+    var checkInterval = 60000;
     var pageContent;
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open('GET', 'http://m.lenta.yandex.ru/unread.xml', false);
     try {
-    	xmlhttp.send(null);
+	xmlhttp.send(null);
     } catch (e){
-    	unautorized = 1;
-    	setTimeout('checkFeed()', checkInterval);
+	unautorized = 1;
+	setTimeout('checkFeed()', checkInterval);
         displayFeed();
-    	return;
+	return;
     }
     if(xmlhttp.status == 200) {
         pageContent = xmlhttp.responseText;
     }
     var startTag = '<a href="./read.xml?group=all">Все подписки</a>';
     var endTag = '</h2>'
-    var startPos = pageContent.indexOf(startTag);
+    try {
+	var startPos = pageContent.indexOf(startTag);
+    } catch (e) {
+	setTimeout('checkFeed()', checkInterval);
+	return;
+    }
     if (startPos == -1 || startPos == undefined ) {
         unautorized = 1;
         setTimeout('checkFeed()', checkInterval);
@@ -60,14 +67,8 @@ function checkFeed(){
     var rawCounter = pageContent.substring(startPos,pageContent.indexOf(endTag));
     count = rawCounter.replace(" ","").replace("(","").replace(")","");
     displayFeed();
+    setTimeout('checkFeed()', checkInterval);
     return;
 }
 
-function runner(){
-	var checkInterval = 60000;
-	setTimeout("runner()", checkInterval);
-	checkFeed();
-}
-
-runner();
-
+checkFeed();
